44830d90bcededcd1052baa05a5d91c3c299dc0a
pokexthecoder <gerardmarquina@gmail.com>
2017-07-27 10:56:36 -0700

    Added things.-.

---
+++ Win 10 Optimizer/--RUN THIS 0--.ps1
@@ -0,0 +1,31 @@
+Get-AppxPackage -Name *WindowsCamera* | Remove-AppxPackage
+
+Get-AppxPackage -Name *ZuneMusic* | Remove-AppxPackage
+
+Get-AppxPackage -Name *WindowsMaps* | Remove-AppxPackage
+
+Get-AppxPackage -Name *MicrosoftSolitaireCollection* | Remove-AppxPackage
+
+Get-AppxPackage -Name *BingFinance* | Remove-AppxPackage
+
+Get-AppxPackage -Name *ZuneVideo* | Remove-AppxPackage
+
+Get-AppxPackage -Name *BingNews* | Remove-AppxPackage
+
+Get-AppxPackage -Name *WindowsPhone* | Remove-AppxPackage
+
+Get-AppxPackage -Name *Windows.Photos* | Remove-AppxPackage
+
+Get-AppxPackage -Name *BingSports* | Remove-AppxPackage
+
+Get-AppxPackage -Name *XboxApp* | Remove-AppxPackage
+
+Get-AppxPackage -Name *BingWeather* | Remove-AppxPackage
+
+Get-AppxPackage -Name *WindowsSoundRecorder* | Remove-AppxPackage
+
+Get-AppxPackage -Name *3DBuilder* | Remove-AppxPackage
+
+Get-AppxPackage -Name *SkypeApp* | Remove-AppxPackage
+
+Get-AppxPackage -Name *MicrosoftOfficeHub* | Remove-AppxPackage
